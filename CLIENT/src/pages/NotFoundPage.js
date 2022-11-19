1511766250d4b5f9d02acbbaf07feb37c343879c
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-11-19 18:48:04 +0600

    add comming and not found page

--- CLIENT/src/pages/NotFoundPage.js
+++ CLIENT/src/pages/NotFoundPage.js
@@ -1,7 +1,33 @@
 import React from "react";
-
+import { AiOutlineDoubleLeft } from "react-icons/ai";
+import { CgSmileSad } from "react-icons/cg";
+import { Link } from "react-router-dom";
 const NotFoundPage = () => {
-    return <div>this is not found page</div>;
+    return (
+        <div className="h-screen flex justify-center items-center bg-neutral">
+            <div className=" px-4 flex justify-center flex-col items-center">
+                <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold tracking-wide uppercase flex items-center text-secondary mb-1 md:mb-3">
+                    !Sorry <CgSmileSad className="text-base-200" />
+                </h2>
+                <p className="font-semibold uppercase text-lg sm:text-xl lg:text-2xl text-base-200 mb-1 md:mb-2">
+                    404 - Page not found
+                </p>
+                <p className="text-base-200 text-sm md:text-base max-w-sm text-center mb-3 md:mb-5">
+                    The page you are looking might have been removed or is
+                    temporarily unavailable
+                </p>
+                <Link
+                    to="/"
+                    className=" flex items-center px-4 py-1 md:px-8 md:py-2 border-2 border-secondary rounded-3xl text-primary uppercase font-medium text-sm md:text-base hover:text-base-100 hover:bg-secondary duration-300 group"
+                >
+                    <AiOutlineDoubleLeft className="font-bold text-primary group-hover:text-base-100 duration-300" />{" "}
+                    <span className="group-hover:ml-1 duration-300">
+                        back to home
+                    </span>
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default NotFoundPage;
