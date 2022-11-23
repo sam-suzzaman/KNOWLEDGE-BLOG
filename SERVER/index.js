219e4df383c76e23ade3988441a61896ca71fc16
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-11-23 09:11:02 +0600

    testing env file

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 // const port = process.env.PORT || 5000;
-const port = 5000;
+const port = 2000;
 
 // app.use(express.json()); // json parsing
 // require("dotenv").config(); // dotenv
