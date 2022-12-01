c607a2ac30614aa244359e88939125e8e6e677ab
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-12-02 00:13:24 +0600

    showing category list to UI

--- CLIENT/src/components/nonShared/CategoryPage/CategoryListCom.js
+++ CLIENT/src/components/nonShared/CategoryPage/CategoryListCom.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { toast } from "react-toastify";
+import LoadingCom from "../../shared/Loading";
 
 const CategoryListCom = () => {
+    const [categoryList, setCategoryList] = useState([]);
+    const [listLoading, setListLoading] = useState(true);
+
+    // fetching category data
+    const fetchCategories = async () => {
+        try {
+            const response = await axios.get(
+                "http://localhost:2000/api/v1/blog/get-category"
+            );
+            if (response?.data?.status) {
+                setCategoryList(response.data.result);
+                setListLoading(false);
+                console.log(categoryList);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
     const {
         register,
         formState: { errors },
@@ -18,6 +41,10 @@ const CategoryListCom = () => {
         // );
         reset();
     };
+
+    if (listLoading) {
+        return <LoadingCom />;
+    }
     return (
         <>
             <div className="w-full max-w-lg max-[768px]:mx-auto">
@@ -30,30 +57,26 @@ const CategoryListCom = () => {
                             action=""
                             onSubmit={handleSubmit(handleFormSubmit)}
                         >
-                            <div className="form-control w-max">
-                                <label className="cursor-pointer label justify-start py-0 mb-1">
-                                    <input
-                                        type="checkbox"
-                                        className="checkbox h-[17px] w-[17px] checkbox-base-100"
-                                        {...register("one")}
-                                    />
-                                    <span className="label-text text-primary text-base">
-                                        Remember me
-                                    </span>
-                                </label>
-                            </div>
-                            <div className="form-control w-max">
-                                <label className="cursor-pointer label justify-start py-0 mb-1">
-                                    <input
-                                        type="checkbox"
-                                        className="checkbox h-[17px] w-[17px] checkbox-base-100"
-                                        {...register("two")}
-                                    />
-                                    <span className="label-text text-primary text-base">
-                                        Remember me
-                                    </span>
-                                </label>
-                            </div>
+                            {categoryList?.map((category) => {
+                                return (
+                                    <div
+                                        className="form-control w-max"
+                                        key={category._id}
+                                    >
+                                        <label className="cursor-pointer label justify-start py-0 mb-1">
+                                            <input
+                                                type="checkbox"
+                                                className="checkbox h-[17px] w-[17px] checkbox-base-100"
+                                                {...register(category._id)}
+                                            />
+                                            <span className="label-text text-primary text-base">
+                                                {category.categoryName}
+                                            </span>
+                                        </label>
+                                    </div>
+                                );
+                            })}
+
                             {/* delete button */}
                             <div className="flex justify-center mt-4">
                                 <button
